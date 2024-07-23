004142b87f4a084338954bbc4c702ad375a6eab2
JuferBlue <2846823009@qq.com>
2024-07-23 14:16:45 +0800

    AlexNet

--- Test.py
+++ Test.py
@@ -6,5 +6,10 @@
 """
 
 
+import os
+
+dir_path = os.path.join(".", "模型保存", "1-LeNet")
+if not os.path.exists(dir_path):
+    os.makedirs(dir_path)
 
 
--- 经典网络/1.LeNet.py
+++ 经典网络/1.LeNet.py
@@ -2,13 +2,22 @@ import torch
 import torchvision
 from torch import nn
 from torch.utils.data import DataLoader
+from torchvision.transforms import ToTensor
+import os
+
+# 利用GPU训练
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+transform = torchvision.transforms.Compose([
+    ToTensor(),
+    torchvision.transforms.Resize(28),
+])
 
 # 准备数据
-train_data = torchvision.datasets.MNIST(root="../数据集/mnist", train=True, transform=torchvision.transforms.ToTensor(),
+train_data = torchvision.datasets.CIFAR10(root="../数据集/cifar10", train=True, transform=transform,
                                         download=True)
-test_data = torchvision.datasets.MNIST(root="../数据集/mnist", train=False, transform=torchvision.transforms.ToTensor(),
+test_data = torchvision.datasets.CIFAR10(root="../数据集/cifar10", train=False, transform=transform,
                                        download=True)
-print(train_data)
 
 # 查看数据集长度
 train_data_size = len(train_data)
@@ -26,7 +35,7 @@ class LeNet(nn.Module):
     def __init__(self):
         super(LeNet, self).__init__()
         self.model = nn.Sequential(
-            nn.Conv2d(1, 6, 5, 1, 2),
+            nn.Conv2d(3, 6, 5, 1, 2),
             nn.Sigmoid(),
             nn.AvgPool2d(2, 2),
             nn.Conv2d(6, 16, 5, 1, 0),
@@ -46,12 +55,19 @@ class LeNet(nn.Module):
 
 # 实例化网络
 lenet = LeNet()
+lenet = lenet.to(device)
+
+# 初始化模型参数
+def init_weights(m):
+    if type(m) == nn.Linear or type(m) == nn.Conv2d:
+        nn.init.xavier_uniform_(m.weight)
+lenet.apply(init_weights)
 
 # 损失函数
 loss_fn = nn.CrossEntropyLoss()
-
+loss_fn = loss_fn.to(device)
 # 优化器
-learning_rate = 1e-2
+learning_rate = 0.1
 optimizer = torch.optim.SGD(lenet.parameters(), lr=learning_rate)
 
 # 设置训练网络的一些参数
@@ -61,7 +77,7 @@ total_train_step = 0
 total_test_step = 0
 
 # 设置训练网络的循环次数
-epoch = 10
+epoch = 100
 
 for i in range(epoch):
     print(f"--------第{i + 1}轮训练开始--------")
@@ -69,6 +85,8 @@ for i in range(epoch):
     lenet.train()
     for data in train_dataloader:
         images, targets = data
+        images = images.to(device)
+        targets = targets.to(device)
         outputs = lenet(images)
         loss = loss_fn(outputs, targets)
 
@@ -79,7 +97,8 @@ for i in range(epoch):
         total_train_step += 1
 
         if total_train_step % 100 == 0:
-            print(f"训练次数{total_train_step},loss:{loss}")
+            print("训练次数{}, loss: {:.4f}".format(total_train_step, loss.item()))
+
 
     # 测试步骤开始
     lenet.eval()
@@ -89,10 +108,12 @@ for i in range(epoch):
     with torch.no_grad():
         for data in test_dataloader:
             images, targets = data
+            images = images.to(device)
+            targets = targets.to(device)
             outputs = lenet(images)
             loss = loss_fn(outputs, targets)
             total_test_loss+=loss.item()
-            accuracy = (outputs.argmax(1) == targets).sum()
+            accuracy = (outputs.argmax(1) == targets).sum().item()
             total_accuracy += accuracy
 
     # 输出一个轮次的信息
@@ -100,8 +121,13 @@ for i in range(epoch):
     print(f"整体测试集上的正确率:{total_accuracy/test_data_size}")
     total_test_step += 1
 
-    # 每一轮的模型
-    torch.save(lenet, f"lenet{i}.pth")
+
+    # 创建文件夹用来保存模型
+    dir_path = os.path.join(".", "模型保存", "1-LeNet")
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+    # 保存每一轮的模型
+    torch.save(lenet.state_dict(), f"./模型保存/1-LeNet/lenet{i}.pth")
     print("模型保存成功")
 
 
