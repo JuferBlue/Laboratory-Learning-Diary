1664ab04d2f31575d01a78391644789a2bf9bf4c
JuferBlue <2846823009@qq.com>
2024-07-24 20:53:14 +0800

    框架搭建

---
+++ 经典网络/训练通用框架/test.py
@@ -0,0 +1,9 @@
+# 导入相关的包
+import torch
+import torchvision
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision.transforms import ToTensor
+import os
+import torch.nn.functional as F
+
